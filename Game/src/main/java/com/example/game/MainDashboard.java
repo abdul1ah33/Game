14822f684a848d4b15275134bc455983e8206899
abdul1ah33 <study.abdullah2005@gmail.com>
2025-12-25 11:56:11 +0200

    dfd

--- Game/src/main/java/com/example/game/MainDashboard.java
+++ Game/src/main/java/com/example/game/MainDashboard.java
@@ -10,6 +10,8 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.List;
+
 public class MainDashboard extends Application {
 
     private BorderPane root;
